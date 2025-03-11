4f374764624631bf304c93237f82adf40e2cb76a
Robert Rudi <robertcortezrudi@gmail.com>
2025-03-11 15:44:38 -0300

    REFACTOR: tests for recipe urls

---
+++ project/recipes/tests/test_recipe_urls.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        url = reverse("codecook:home")
+        self.assertEqual(url, "/") 
+
+    def test_recipe_categoy_url_is_correct(self):
+        url = reverse("codecook:category", kwargs={"category_id": 1})
+        self.assertEqual(url, "/recipes/category/1/") 
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse("codecook:recipe", kwargs={"id": 1})
+        self.assertEqual(url, "/recipes/1/") 
