878c0550ad62f32f872551ba499023d6c0c2705d
Robert Rudi <robertcortezrudi@gmail.com>
2025-03-18 11:13:09 -0300

    TEST: testing the string representation and max_length of the model category

---
+++ project/recipes/tests/test_recipe_model_category.py
@@ -0,0 +1,15 @@
+from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
+
+class CategoryModelTest(RecipeTestBase):
+    def setUp(self):
+        self.category = self.make_category(name="Testing")
+        return super().setUp()
+
+    def test_recipe_category_model_string_representation_is_name_field(self):
+        self.assertEqual(str(self.category), self.category.name)
+
+    def test_recipe_category_model_name_max_length_is_65_chars(self):
+        self.category.name = "R" * 70
+        with self.assertRaises(ValidationError):
+            self.category.full_clean()
