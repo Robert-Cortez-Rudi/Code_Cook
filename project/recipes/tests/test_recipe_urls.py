67f79b1142fdb162add89fa68adfc52a8d959f58
Robert Rudi <robertcortezrudi@gmail.com>
2025-03-19 17:21:53 -0300

    TEST: test for search url

--- project/recipes/tests/test_recipe_urls.py
+++ project/recipes/tests/test_recipe_urls.py
@@ -13,3 +13,7 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse("codecook:recipe", kwargs={"id": 1})
         self.assertEqual(url, "/recipes/1/") 
+
+    def test_recipe_search_url_is_correct(self):
+        url = reverse("codecook:search")
+        self.assertEqual(url, "/recipes/search/") 
