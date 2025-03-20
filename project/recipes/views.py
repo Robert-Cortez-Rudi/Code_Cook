4c80dfbbd4c0880379a862cb3b6648706e069b6d
Robert Rudi <robertcortezrudi@gmail.com>
2025-03-20 17:25:47 -0300

    FEAT: function implementation and returns in the view function search

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
+from django.http import Http404
+from django.db.models import Q
 from .models import Recipe
 
 def home(request):
@@ -26,4 +27,18 @@ def recipe(request, id):
     })
 
 def search(request):
-    return render(request, "recipes/pages/search.html")
+    search_term = request.GET.get("search", "").strip()
+    
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains = search_term) |
+        Q(description__icontains = search_term)
+    ).order_by("-id")
+
+    return render(request, "recipes/pages/search.html", {
+        "page_title": f"Search for '{search_term}'",
+        "search_term": search_term,
+        "recipes": recipes
+    })
