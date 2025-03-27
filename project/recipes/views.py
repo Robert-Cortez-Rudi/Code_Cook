b000e9bc725cb45892776af0c0b56f0d48c3a070
Robert Rudi <robertcortezrudi@gmail.com>
2025-03-26 22:43:45 -0300

    FEAT: insertion pagination in view function

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import Http404
 from django.db.models import Q
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination
 from .models import Recipe
+
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by("-id")
+
+    page_object, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, "recipes/pages/home.html", context= {
-        "recipes": recipes
+        "recipes": page_object,
+        "pagination_range": pagination_range
     })
 
 
@@ -14,10 +21,15 @@ def category(request, category_id):
     recipes = get_list_or_404(
         Recipe.objects.filter(category__id=category_id, is_published=True).order_by("-id")
     )
+
+    page_object, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, "recipes/pages/category.html", context= {
-        "recipes": recipes,
+        "recipes": page_object,
+        "pagination_range": pagination_range,
         "title": f"{recipes[0].category.name} - Category"
     })
+
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, id=id, is_published=True)
@@ -25,6 +37,7 @@ def recipe(request, id):
         "recipe": recipe,
         "is_detail_page": True
     })
+
 
 def search(request):
     search_term = request.GET.get("search", "").strip()
@@ -40,8 +53,12 @@ def search(request):
         is_published = True
     ).order_by("-id")
 
+    page_object, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, "recipes/pages/search.html", {
         "page_title": f"Search for '{search_term}'",
         "search_term": search_term,
-        "recipes": recipes
+        "recipes": page_object,
+        "pagination_range": pagination_range,
+        "additional_url_query": f"&search={search_term}"
     })
