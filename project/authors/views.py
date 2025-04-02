ac14515b406e67b83186b4ab60ae5921038346af
Robert Rudi <robertcortezrudi@gmail.com>
2025-04-02 13:47:54 -0300

    FEAT: Submitting the form to the database and cleaning it

--- project/authors/views.py
+++ project/authors/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import RegisterForm
 from django.http import Http404
 
@@ -17,6 +18,12 @@ def register_create(request):
     POST = request.POST
     request.session["register_form_data"] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Your user is created, please login")
+        del(request.session["register_form_data"])
+
 
     return redirect("authors:register")
 
