ea0ddb44ff440ee866e3b00f312f2a30896c13c5
Robert Rudi <robertcortezrudi@gmail.com>
2025-04-02 13:45:58 -0300

    FEAT: form field validation

--- project/authors/forms.py
+++ project/authors/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
 
 def add_attrs(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, "")
@@ -9,6 +11,15 @@ def add_attrs(field, attr_name, attr_new_val):
 def add_placeholder(field, placeholder_val):
     field.widget.attrs["placeholder"] = f"{placeholder_val}".strip()
 
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    
+    if not regex.match(password):
+        raise ValidationError(
+            """Password must have at least one uppercase letter, one lowercase letter and one number.
+            The length should be at least 8 characters.""", code="invalid")
+    
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -28,7 +39,8 @@ class RegisterForm(forms.ModelForm):
         help_text= (
             "Password must have at least one uppercase letter, one lowercase letter and one number. ",
             "The length should be at least 8 characters."
-        )
+        ),
+        validators= [strong_password]
     )
 
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
@@ -65,4 +77,19 @@ class RegisterForm(forms.ModelForm):
                 "placeholder": "Type your password here"
             })
         }
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password2")
 
+        if password != password2:
+            password_error = ValidationError(
+                    "Password and password2 must be equal",
+                    code="invalid"
+                )
+            raise ValidationError({
+                "password" : password_error,
+                "password2" : password_error
+            })
+    
