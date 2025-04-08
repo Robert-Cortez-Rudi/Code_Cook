3a8dfce27a25d66067a3eec8f1fe3ef6c6b4cb8e
Robert Rudi <robertcortezrudi@gmail.com>
2025-04-08 13:56:35 -0300

    REFACTOR: adjustment in password validation message

--- project/authors/forms.py
+++ project/authors/forms.py
@@ -16,9 +16,12 @@ def strong_password(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
     
     if not regex.match(password):
-        raise ValidationError(
-            """Password must have at least one uppercase letter, one lowercase letter and one number.
-            The length should be at least 8 characters.""", code="invalid")
+        raise ValidationError((
+            "Password must have at least one uppercase letter, "
+            "one lowercase letter and one number. "
+            "The length should be at least 8 characters."   
+        ), 
+        code="invalid")
     
 
 class RegisterForm(forms.ModelForm):
