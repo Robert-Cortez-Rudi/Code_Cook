c5740f3c6ff46152fc82865f481811772cf683e1
Robert Rudi <robertcortezrudi@gmail.com>
2025-04-13 16:09:38 -0300

    FEAT: implementation logout view function

--- project/authors/views.py
+++ project/authors/views.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 from django.contrib import messages
 from .forms import RegisterForm, LoginForm
 from django.http import Http404
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def register_view(request):
     register_form_data = request.session.get("register_form_data", None)
@@ -65,6 +66,13 @@ def login_create(request):
     
     return redirect(login_url)
 
+@login_required(login_url="authors:login", redirect_field_name="next")
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse("authors:login"))
 
-def logout(request):
-    ...
+    if request.POST.get("username") != request.user.username:
+        return redirect(reverse("authors:login"))
+
+    logout(request)
+    return redirect(reverse("authors:login"))
