c8ffb671932a1e08f6bd35ff61c6fd5b363a69a1
Robert Rudi <robertcortezrudi@gmail.com>
2025-04-23 13:35:06 -0300

    BUILD: function created to generate functional tests with selenium and its configurations

---
+++ utils/browser.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+
+ROOT_PATH = Path(__file__).parent.parent
+CHROME_DRIVER_NAME = "chromedriver-win64/chromedriver.exe"
+CHROME_DRIVER_PATH = ROOT_PATH / "bin" / CHROME_DRIVER_NAME
+
+
+def make_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(executable_path=CHROME_DRIVER_PATH)
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return browser
+
+
+if __name__ == "__main__":
+    browser = make_browser('--headless')
+    browser.get("https://www.github.com/")
+
+    sleep(5)
+
+    browser.quit()
+
