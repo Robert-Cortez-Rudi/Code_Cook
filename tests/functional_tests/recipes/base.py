d1f652a939356bc1580e31dfb5f482fcd107ee45
Robert Rudi <robertcortezrudi@gmail.com>
2025-04-24 22:42:24 -0300

    FEAT: Recipe base class for functional tests - Multiple inheritance with RecipeMixin to create recipes for data-intensive tests

---
+++ tests/functional_tests/recipes/base.py
@@ -0,0 +1,16 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from utils.browser import make_browser
+from time import sleep
+from project.recipes.tests.test_recipe_base import RecipeMixin
+
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase, RecipeMixin):
+    def setUp(self):
+        self.browser = make_browser()
+        return super().setUp()
+    
+    def tearDown(self):
+        self.browser.quit()
+        return super().tearDown()
+
+    def sleep(self, seconds = 5):
+        sleep(seconds)
