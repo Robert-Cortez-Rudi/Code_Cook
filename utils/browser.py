7932aefd32f0404789e1adf791548472f8864b55
Robert Rudi <robertcortezrudi@gmail.com>
2025-04-24 22:33:49 -0300

    FEATURE: update in make_browser with configuration to enable headless with environment variable

--- utils/browser.py
+++ utils/browser.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from time import sleep
+import os
 
 ROOT_PATH = Path(__file__).parent.parent
 CHROME_DRIVER_NAME = "chromedriver-win64/chromedriver.exe"
@@ -14,6 +15,9 @@ def make_browser(*options):
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+
+    if os.environ.get("SELENIUM_HEADLESS") == 1:
+        chrome_options.add_argument("--headless")
 
     chrome_service = Service(executable_path=CHROME_DRIVER_PATH)
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
