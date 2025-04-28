d94a314ee8672541f434ff733832a81f616bf7ac
Robert Rudi <robertcortezrudi@gmail.com>
2025-04-28 17:29:30 -0300

    FEAT:  insertion of the get_by_id method

--- tests/functional_tests/authors/base.py
+++ tests/functional_tests/authors/base.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from utils.browser import make_browser
 from time import sleep
+from selenium.webdriver.common.by import By
 
 class AuthorsBaseTest(StaticLiveServerTestCase):
     def setUp(self):
@@ -13,3 +14,14 @@ class AuthorsBaseTest(StaticLiveServerTestCase):
 
     def sleep(self, seconds = 5):
         sleep(seconds)
+
+    def get_by_id(self, web_element, id):
+        return web_element.find_element(
+            By.XPATH,
+            id
+        )
+
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder}"]'
+        )
