7f8f2b23a2709144648709fd7423eec4f8d087a5
Robert Rudi <robertcortezrudi@gmail.com>
2025-05-07 21:41:12 -0300

    FEAT: validation of the editing form with its respective data and sending it to the database - Creation of a view to delete a recipe

--- project/authors/views.py
+++ project/authors/views.py
@@ -107,9 +107,71 @@ def dashboard_recipe_edit(request, id):
 
     form = AuthorRecipeForm(
         request.POST or None,
+        files=request.FILES or None,
         instance=recipe
     )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, "Sua receita foi salva com sucesso!")
+        return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
 
     return render(request, "authors/pages/dashboard_recipe.html",  context={
         "form": form
     })
+
+
+@login_required(login_url="authors:login", redirect_field_name="next")
+def dashboard_recipe_create(request):
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, "Sua receita foi criada com sucesso!")
+        return redirect(reverse("authors:dashboard"))
+
+    return render(request, "authors/pages/dashboard_recipe.html",  context={
+        "form": form,
+        "form_action": reverse("authors:dashboard_recipe_create")
+    })
+
+
+@login_required(login_url="authors:login", redirect_field_name="next")
+def dashboard_recipe_delete(request):
+    if not request.POST:
+        raise Http404()
+    
+    POST = request.POST
+    id = POST.get("id")
+
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=False,
+        author=request.user
+    ).first()
+
+    if not recipe:
+        raise Http404()
+    
+    recipe.delete()
+
+    messages.success(request, "Deleted successfully!")
+
+    return redirect(reverse("authors:dashboard"))
