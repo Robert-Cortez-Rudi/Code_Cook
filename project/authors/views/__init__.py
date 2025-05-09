f5e22e9689d0d3f04066a388c7fddddf04a06e4e
Robert Rudi <robertcortezrudi@gmail.com>
2025-05-09 13:21:00 -0300

    __init__.py from views/

---
+++ project/authors/views/__init__.py
@@ -0,0 +1,4 @@
+from project.authors.views.login import login_view, login_create
+from project.authors.views.register import register_view, register_create
+from project.authors.views.logout import logout_view
+from project.authors.views.dashboard import dashboard, dashboard_recipe_create, dashboard_recipe_delete, dashboard_recipe_edit
