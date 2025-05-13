dea69934570688fc3a371b51d3142464502aa00e
Robert Rudi <robertcortezrudi@gmail.com>
2025-05-13 13:12:44 -0300

    FEAT: change from FBV to CBV on the system home page

--- project/recipes/urls.py
+++ project/recipes/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "codecook"
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.RecipeListViewHome.as_view(), name="home"),
     path("recipes/category/<int:category_id>/", views.category, name="category"),
     path("recipes/<int:id>/", views.recipe, name="recipe"),
     path("recipes/search/", views.search, name="search")
